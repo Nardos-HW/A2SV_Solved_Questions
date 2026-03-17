cc713c24f4e8ba4aca70c656827a4836082ce5c9
Nardos Haile Wubeneh <nardos.ugr-0410-16@aau.edu.et>
2026-03-17 19:57:06 +0300

    Time: 35 ms (52.83%) | Memory: 23.4 MB (9.60%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        
+        count = 0
+        prefix_sum = 0
+        freq = defaultdict(int)
+        freq[0] = 1
+
+        for num in nums:
+            prefix_sum += num
+            count += freq[prefix_sum - goal]
+            freq[prefix_sum] += 1
+
+        return count
