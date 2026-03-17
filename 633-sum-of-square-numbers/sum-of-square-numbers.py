11b752cb3d82f4b6b249dd8c38bdbd2632b95bda
Nardos Haile Wubeneh <nardos.ugr-0410-16@aau.edu.et>
2026-03-17 11:48:12 +0300

    Time: 52 ms (93.41%) | Memory: 19.4 MB (83.88%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -0,0 +1,14 @@
+import math
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        
+        a, b = 0, int(math.isqrt(c))
+        while a <= b:
+            total = a * a + b * b
+            if total == c:
+                return True
+            elif total < c:
+                a += 1
+            else:
+                b -= 1
+        return False
